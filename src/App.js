651e45ffe2a291934e6a36f9f7ca7574a4f33c89
chheang-pheaktra <chheangpheaktra154@gmail.com>
2023-12-11 16:44:57 +0700

    project frontend with reat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./page/Home";
+import Login from "./page/login";
+import Signup from "./page/signup";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+     <Login/>  
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/page/signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+const Signup = () => {
+    return (
+        <div>
+            
+            Hello world
+        </div>
+    );
+}
+
+export default Signup;
